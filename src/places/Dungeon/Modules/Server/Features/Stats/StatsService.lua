0fd1598269975362abe728abd7b5a8457cfa64e5
samdev22 <samjaytaylor22@gmail.com>
2026-01-17 12:03:34 -0600

    Stats Controller removed. Updated StatsService and Types to accommodate PrimaryStats directly.

--- src/places/Dungeon/Modules/Server/Features/Stats/StatsService.lua
+++ src/places/Dungeon/Modules/Server/Features/Stats/StatsService.lua
@@ -12,9 +12,12 @@ local require = require(script.Parent.loader).load(script)
 -- [ Imports ] --
 local ServiceBag = require("ServiceBag")
 local StatTypes = require("StatsTypes")
-local EnemyConfigs = require("EnemyConfigs")
+local StatsCalculationUtility = require("StatsCalculationUtility")
 
 -- [ Constants ] --
+local GET_PLAYER_RAW_STATS = "GetPlayerRawStats"
+local GET_PLAYER_PRIMARY_STATS = "GetPlayerPrimaryStats"
+local GET_STATS_SERVICE_CHANNEL = "StatsServiceChannel"
 
 -- [ Variables ] --
 
@@ -25,6 +28,7 @@ local CombatService = {}
 type ModuleData = {
     _ServiceBag: ServiceBag.ServiceBag,
     _NetworkService: typeof(require("NetworkService")),
+    _DataService :  typeof(require("DataService")),
 }
 
 export type Module = typeof(CombatService) & ModuleData
@@ -39,10 +43,36 @@ function CombatService.Init(self: Module, serviceBag: ServiceBag.ServiceBag)
 
     self._ServiceBag = assert(serviceBag, "No serviceBag")
     self._NetworkService = self._ServiceBag:GetService(require("NetworkService"))
+    self._DataService = self._ServiceBag:GetService(require("DataService"))
 end
 
 function CombatService.Start(self: Module)
-    print(EnemyConfigs)
+    local networkChannel = self._NetworkService:GetNetwork(GET_STATS_SERVICE_CHANNEL)
+    
+    networkChannel:Bind(GET_PLAYER_PRIMARY_STATS, function(player : Player)
+        local isValid, data : StatTypes.PrimaryStats = self._DataService:GetData(player,  "PrimaryStats")
+        if not isValid then
+            return warn(`No data for player {player.Name}`)
+        end
+
+        return table.clone(data)
+    end)
+
+    networkChannel:Bind(GET_PLAYER_RAW_STATS, function(player : Player)
+        local isValid, data : StatTypes.PrimaryStats = self._DataService:GetData(player,  "RawStats")
+        if not isValid then
+            return warn(`No data for player {player.Name}`)
+        end
+
+        local stat : StatTypes.PlayerStats = {
+            RawStats = {} :: StatTypes.RawStats,
+            PrimaryStats = data :: StatTypes.PrimaryStats,
+        }
+
+        StatsCalculationUtility:UpdateStats(stat)
+
+        return table.clone(stat.RawStats)
+    end)
 end
 
 return CombatService :: Module
